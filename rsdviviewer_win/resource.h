031a6a3a81b3cf421e86968cdf72273a9093a9de
nadder <mylodon@gmail.com>
2019-01-06 12:20:15 +0100

    printing multiple pages

--- rsdviviewer_win/resource.h
+++ rsdviviewer_win/resource.h
@@ -5,10 +5,11 @@
 #define IDR_MENU1                       101
 #define IDD_SETRESOLUTION               102
 #define IDD_SETPAGESIZE                 104
+#define IDD_GOTOPAGE                    106
 #define IDC_EDIT1                       1001
 #define IDC_EDIT_WIDTH                  1002
-#define IDC_EDIT_WIDTH2                 1003
 #define IDC_EDIT_HEIGHT                 1003
+#define IDC_EDIT_PAGE                   1003
 #define ID_FILE_OPEN                    40001
 #define ID_FILE_EXIT                    40002
 #define ID_FILE_ZOOMIN                  40003
@@ -17,14 +18,16 @@
 #define ID_FILE_NEXTPAGE                40006
 #define ID_FILE_PREVPAGE                40007
 #define ID_FILE_SETPAGESIZE             40008
+#define ID_FILE_GOTOPAGE                40009
+#define ID_FILE_PRINT                   40010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40009
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
