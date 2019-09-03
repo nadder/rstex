dc68ba999b790cdc2a6b40e56404836a0ed6c371
nadder <mylodon@gmail.com>
2019-09-03 22:00:02 +0200

    adding one more build

--- rsmetafont_win/build.bat
+++ rsmetafont_win/build.bat
@@ -1,3 +1,4 @@
 cl.exe /EHsc /Fecpf /std:c++14 ..\CreatePoolFile\CreatePoolFile.cpp
 cpf.exe TEX_STRING rsMetaFont.cpp.pre rsMetaFont.h.pre
+ren tex.pool mf.pool
 msbuild -property:Configuration=Debug rsMetaFont.vcxproj
