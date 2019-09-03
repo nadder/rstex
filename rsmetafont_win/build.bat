889a2c4e38c9c2c3db4b9fb293c7c546945b2caf
nadder <mylodon@gmail.com>
2019-09-03 21:12:36 +0200

    trying again

--- rsmetafont_win/build.bat
+++ rsmetafont_win/build.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+"call C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 cl /EHsc /Fecpf /std:c++14 ..\CreatePoolFile\CreatePoolFile.cpp
 cpf TEX_STRING rsMetaFont.cpp.pre rsMetaFont.h.pre
 msbuild -property:Configuration=Debug rsMetaFont.vcxproj
