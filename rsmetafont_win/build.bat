fe2c0fabf8004132710d00cf217618bb8cf5b5f1
nadder <mylodon@gmail.com>
2019-09-03 21:29:21 +0200

    trying again..

--- rsmetafont_win/build.bat
+++ rsmetafont_win/build.bat
@@ -1,3 +1,3 @@
-cl.exe /EHsc /Fecpf /std:c++14 ..\CreatePoolFile\CreatePoolFile.cpp'
+cl.exe /EHsc /Fecpf /std:c++14 ..\CreatePoolFile\CreatePoolFile.cpp
 cpf.exe TEX_STRING rsMetaFont.cpp.pre rsMetaFont.h.pre
 msbuild -property:Configuration=Debug rsMetaFont.vcxproj
