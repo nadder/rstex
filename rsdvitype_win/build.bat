c2af9132415c42eeef6f7fca8f9a6dc706e32600
nadder <mylodon@gmail.com>
2019-09-03 23:23:28 +0200

    Adding more builds

---
+++ rsdvitype_win/build.bat
@@ -0,0 +1,1 @@
+cl.exe /EHsc rsDviType.cpp
---
+++ rstex_win/build.bat
@@ -0,0 +1,4 @@
+cl.exe /EHsc /Fecpf /std:c++14 ..\CreatePoolFile\CreatePoolFile.cpp
+cpf.exe TEX_STRING rstex.cpp.pre rstex.h.pre
+cl.exe /EHsc rstex.cpp
+
