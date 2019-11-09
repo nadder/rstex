c299ecde93543d36ce8e2dec5ae280f44b25e53c
nadder <mylodon@gmail.com>
2019-11-09 23:27:09 +0100

    fixed path in build script

--- rsmetafont_unix/build.sh
+++ rsmetafont_unix/build.sh
@@ -1,3 +1,3 @@
-g++ -o cpf -std=c++14 CreatePoolFile/CreatePoolFile.cpp
+g++ -o cpf -std=c++14 ../CreatePoolFile/CreatePoolFile.cpp
 ./cpf TEX_STRING rsMetaFont.cpp.pre rsMetaFont.h.pre
-g++ -o rsmf -g -Wno-dangling-else rsMetaFont.cpp
+g++ -o rsmf -std=c++14 -O3 -Wno-dangling-else rsMetaFont.cpp
