154b355901f71895e8bfec04f6d684400a30fedb
nadder <mylodon@gmail.com>
2020-06-07 02:19:23 +0200

    abu

--- rsgftodvi_win/build.bat
+++ rsgftodvi_win/build.bat
@@ -1,3 +1,4 @@
+"C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build/vcvarsall.bat" amd64
 cl /EHsc /Fecpf /std:c++14 ..\CreatePoolFile\CreatePoolFile.cpp
 cpf TEX_STRING rsGFtoDVI.cpp.pre rsGFtoDVI.h.pre
 cl /EHsc rsGFtoDVI.cpp
