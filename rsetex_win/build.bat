8da40253a1c29e7f6480037543db03d488d59e07
nadder <mylodon@gmail.com>
2024-01-03 16:32:36 +0100

    Fix build script for etex on win

--- rsetex_win/build.bat
+++ rsetex_win/build.bat
@@ -1,5 +1,5 @@
 "C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build/vcvarsall.bat" amd64
 cl /EHsc /Fecpf /std:c++17 ..\CreatePoolFile\CreatePoolFile.cpp
 cpf TEX_STRING rsetex.cpp.pre rsetex.h.pre
-cl /EHsc /std:c++17 rstex.cpp
+cl /EHsc /std:c++17 rsetex.cpp
 
