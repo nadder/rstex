ae87aea318f3e336d3cf136de190dff954471bb7
nadder <mylodon@gmail.com>
2024-01-05 03:32:52 +0100

    Change to std=c++11 for old gcc

--- rsmetafont_unix/build.sh
+++ rsmetafont_unix/build.sh
@@ -1,4 +1,4 @@
-g++ -o cpf -std=c++14 ../CreatePoolFile/CreatePoolFile.cpp
+g++ -o cpf -std=c++11 ../CreatePoolFile/CreatePoolFile.cpp
 ./cpf TEX_STRING rsMetaFont.cpp.pre rsMetaFont.h.pre
 mv tex.pool mf.pool
-g++ -o rsmf -std=c++14 -O3 -Wno-unused-result -Wno-dangling-else rsMetaFont.cpp
+g++ -o rsmf -std=c++11 -O3 -Wno-unused-result -Wno-dangling-else rsMetaFont.cpp
