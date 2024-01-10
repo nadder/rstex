e308a5c79f2e9e4c69ffa0b7c8cfaea5dd3e399b
nadder <mylodon@gmail.com>
2024-01-10 09:13:16 +0100

    Added call to windows build scripts

--- rsetex_win/build.bat
+++ rsetex_win/build.bat
@@ -1,5 +1,4 @@
-"C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build/vcvarsall.bat" amd64
+call "C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build/vcvarsall.bat" amd64
 cl /EHsc /Fecpf /std:c++17 ..\CreatePoolFile\CreatePoolFile.cpp
 cpf TEX_STRING rsetex.cpp.pre rsetex.h.pre
 cl /EHsc /std:c++17 rsetex.cpp
-
--- rsmetafont_win/build.bat
+++ rsmetafont_win/build.bat
@@ -1,4 +1,4 @@
-"C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build/vcvarsall.bat" amd64
+call "C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build/vcvarsall.bat" amd64
 cl /EHsc /Fecpf /std:c++14 ..\CreatePoolFile\CreatePoolFile.cpp
 cpf TEX_STRING rsMetaFont.cpp.pre rsMetaFont.h.pre
 ren tex.pool mf.pool
